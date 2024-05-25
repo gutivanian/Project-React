2def92f52b75323d85a263e3c857de888ef6223f
gutivanian <gutivanaliefs@gmail.com>
2024-05-25 12:37:35 +0700

    personal-web

--- Personal-Web/src/components/About/SkillModal/PythonModal.jsx
+++ Personal-Web/src/components/About/SkillModal/PythonModal.jsx
@@ -53,43 +53,45 @@ function PythonModal(props) {
             </Row>
             <Row className='carousel-row'>
               <span>My Project with Python</span>
-              <Carousel>
-                <Carousel.Item>
-                  <img 
-                    className="d-block w-100"
-                    src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                    alt="Image One"
-                  /> 
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img 
-                    className="d-block w-100"
-                    src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                    alt="Image One"
-                  /> 
-                  <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img 
-                    className="d-block w-100"
-                    src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                    alt="Image One"
-                  /> 
-                  <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>  
+              <div className='carousel-container'>
+                <Carousel>
+                  <Carousel.Item>
+                    <img 
+                      className="d-block w-100"
+                      src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                      alt="Image One"
+                    /> 
+                    <Carousel.Caption>
+                      <h3>First slide label</h3>
+                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img 
+                      className="d-block w-100"
+                      src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                      alt="Image One"
+                    /> 
+                    <Carousel.Caption>
+                      <h3>Second slide label</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img 
+                      className="d-block w-100"
+                      src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                      alt="Image One"
+                    /> 
+                    <Carousel.Caption>
+                      <h3>Third slide label</h3>
+                      <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                      </p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>  
+              </div>
             </Row>
         </Container>
       </Modal.Body>
