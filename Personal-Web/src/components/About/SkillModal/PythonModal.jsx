ffd1bacd0cebebe84d35f1a3bb04fde47fee6e87
gutivanian <gutivanaliefs@gmail.com>
2024-05-26 13:03:44 +0700

    Modal Selesai, tapi belum di expand ke yang lain selain python

--- Personal-Web/src/components/About/SkillModal/PythonModal.jsx
+++ Personal-Web/src/components/About/SkillModal/PythonModal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Modal, Button, Container, Row, Col } from 'react-bootstrap';
 import python from "../../../assets/python.png"
 import Carousel from 'react-bootstrap/Carousel'; 
+import utbk from "../../../assets/project/utbk.jpg"
 
 function PythonModal(props) {
   return (
@@ -13,11 +14,8 @@ function PythonModal(props) {
       </Modal.Header>
       <Modal.Body>
         <Container>
-            <Row>
-            Python is a versatile, high-level programming language known for its readability and ease of use, making it ideal for both beginners and experienced developers. It is widely used across various domains, including web development, data analysis, artificial intelligence, scientific computing, and automation.
-            </Row>
-            <Row>
-                <Col className="modal-container modal-text" sm = {4}>
+            <Row className='skill-level'>
+                <Col className="modal-container" sm = {4}>
                     <img src={python} className='modal-logo' />
                 </Col>
                 <Col className="modal-container modal-text" sm = {8}>
@@ -25,18 +23,14 @@ function PythonModal(props) {
                   <Row>
                     <span className='skill-title skill-text'>My skill level</span>
                     <div className='skill-bar'>
-                      <div className="advanced">
-                        
-                      </div>
+                      <div className="advanced"></div>
                     </div>
                   </Row>
                   <br />
                   <Row>
                     <span className='skill-title skill-text'>Skill Use Rate</span>
                     <div className='skill-bar'>
-                      <div className="advanced">
-                        
-                      </div>
+                      <div className="advanced"></div>
                     </div>
                   </Row>
                   <br />
@@ -46,7 +40,7 @@ function PythonModal(props) {
                   <br />
                   <Row>
                     <span className='skill-text'>I commonly use python for:</span>
-                    <ul>
+                    <ul className='modal-list'>
                       <li>Data Analysis</li>
                       <li>Otomatization</li>
                       <li>Numerical Engineering</li>
@@ -55,44 +49,53 @@ function PythonModal(props) {
                   </Row>
                 </Col>
             </Row>
+            {/* <br /> */}
+            <br />
             <Row className='carousel-row'>
-              <span>My Project with Python</span>
+              <br />
+              {/* <br /> */}
+              <h3>My Project with Python</h3>
+              <br/>
               <div className='carousel-container'>
                 <Carousel>
                   <Carousel.Item>
-                    <img 
-                      className="d-block w-100"
-                      src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                      alt="Image One"
-                    /> 
-                    <Carousel.Caption>
-                      <h3>First slide label</h3>
-                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    <a href='https://github.com/gutivanian/UTBK-2019-Analisis-Proses-Seleksi'>
+                      <img 
+                        className="d-block w-100"
+                        src= {utbk}
+                        alt="Image One"
+                      /> 
+                    </a>
+                    <div className="carousel-caption-custom">
+                      <a href='https://github.com/gutivanian/UTBK-2019-Analisis-Proses-Seleksi'>
+                        UTBK 2019 Data Analysis
+                      </a>
+                      {/* <p>In this notebook, I will explore the 2019 UTBK results data. We will start from cleaning and transforming the data for further processing: visualizing to find out the capacity for new student admissions given by PTN through the UTBK route.</p> */}
+                    </div>
                   </Carousel.Item>
                   <Carousel.Item>
                     <img 
                       className="d-block w-100"
                       src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                      alt="Image One"
+                      alt="Second slide"
                     /> 
-                    <Carousel.Caption>
-                      <h3>Second slide label</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    <div className="carousel-caption-custom">
+                      <h5>Second slide label</h5>
+                      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+                    </div>
                   </Carousel.Item>
                   <Carousel.Item>
                     <img 
                       className="d-block w-100"
                       src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                      alt="Image One"
+                      alt="Third slide"
                     /> 
-                    <Carousel.Caption>
-                      <h3>Third slide label</h3>
-                      <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                      </p>
-                    </Carousel.Caption>
+                    <div className="carousel-caption-custom">
+                      <a href='https://github.com/gutivanian/UTBK-2019-Analisis-Proses-Seleksi'>
+                        <h5>Third slide label</h5>
+                      </a>
+                      {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+                    </div>
                   </Carousel.Item>
                 </Carousel>  
               </div>
